19aee86f1db56e2b67cb6b753dc80aeb7830b36d
kitano <kitano>
2023-10-17 00:07:00 +0900

    init commit

---
+++ version_check.py
@@ -0,0 +1,11 @@
+import torch
+import torchvision
+from mmcv.ops import get_compiler_version, get_compiling_cuda_version
+
+import mmpose
+
+print("torch version:", torch.__version__, torch.cuda.is_available())
+print("torchvision version:", torchvision.__version__)
+print("mmpose version:", mmpose.__version__)
+print("cuda version:", get_compiling_cuda_version())
+print("compiler information:", get_compiler_version())
