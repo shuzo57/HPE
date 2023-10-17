462e76029dc6d75c1bff2df5f00e0f3026e1b553
kitano <kitano>
2023-10-18 00:41:08 +0900

    rename: variables

--- SkeltonPose/skeltonpose/utils.py
+++ SkeltonPose/skeltonpose/utils.py
@@ -4,21 +4,21 @@ from mmdet.structures.det_data_sample import DetDataSample
 
 def process_mmdet_results(
     mmdet_results: DetDataSample,
-    person_id: int = 0,
-    person_threshold: float = 0.65,
+    class_id: int = 0,
+    class_threshold: float = 0.65,
 ) -> np.ndarray:
     pred_instances = mmdet_results.pred_instances
 
-    person_results = []
+    class_results = []
     for box, label, score in zip(
         pred_instances.bboxes,
         pred_instances.labels,
         pred_instances.scores,
     ):
-        if label == person_id and score > person_threshold:
+        if label == class_id and score > class_threshold:
             box = box.cpu().numpy()
-            person_results.append(box)
-        if score < person_threshold:
+            class_results.append(box)
+        if score < class_threshold:
             break
 
-    return np.array(person_results)
+    return np.array(class_results)
